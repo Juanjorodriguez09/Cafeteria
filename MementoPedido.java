d42e3f073df3e2ca16963ef2891372ad2efb1b4a
Juan Jose Rodriguez <jrodriguez4452@cue.edu.co>
2025-05-15 08:37:15 -0500

    Actualización del patrón Memento

--- MementoPedido.java
+++ MementoPedido.java
@@ -1,12 +1,17 @@
-
 public class MementoPedido {
     private String estado;
+    private String detalles;
 
-    public MementoPedido(String estado) {
+    public MementoPedido(String estado, String detalles) {
         this.estado = estado;
+        this.detalles = detalles;
     }
 
     public String getEstado() {
         return estado;
+    }
+
+    public String getDetalles() {
+        return detalles;
     }
 }
